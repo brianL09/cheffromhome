cbf8e8e1c32cb0e98bbac1422de5821dc2ee4955
Brian Leighton <bleighton09@gmail.com>
2021-01-26 13:35:07 -0500

    wowie

--- client/src/actions/index.js
+++
@@ -1,4 +0,0 @@
-export const fetchRecipes = () => (dispatch) => {
-    console.log('action fired');
-    dispatch({type: "FETCH_RECIPES", payload:["test"]});
-}
--- client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(
-    ["/create/new"],
-    createProxyMiddleware({
-      target: "http://localhost:5000",
-    })
-  );
-};
