c9a21dba742bc6cd530a0a5a23a3e9aaae03f9b5
Brian Leighton <bleighton09@gmail.com>
2021-02-27 17:58:25 -0500

    updated

--- server/utils/mongoUtil.js
+++ server/utils/mongoUtil.js
@@ -6,7 +6,8 @@ var _db;
 module.exports = {
     connect: function(callback){
         MongoClient.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
-            if(err){console.log("BRIAN HERddE:", keys)}
+            if(err){console.log("BRIAN HERddE:", keys.mongoURI, err)}
+            console.log('connected to mongo');
             _db = client.db('recipe');
             return callback(client, err);
         });
