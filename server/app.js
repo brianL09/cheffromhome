117e71ccad5105a495c27040deaeb72cf17ae038
Brian Leighton <bleighton09@gmail.com>
2021-03-20 13:43:24 -0400

    heroku tests

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ require("./routes/recipeDbRoutes.js")(app);
 if (process.env.NODE_ENV === "production") {
     // Express will serve up production assets
     // like our main.js file, or main.css file
-    app.use(express.static(__dirname, "client/build"));
+    app.use(express.static("client/build"));
     console.log('in production');
     const path = require("path");
     app.get("*", (req, res) => {
